6f4f48a27d408c10000c903f652c73d497eec640
Aurélien Brison <54946149+Aurelien-Brison@users.noreply.github.com>
2020-07-10 11:51:35 +0200

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,5 +2,5 @@ const account = require('./account/lib.js');
 
 module.exports = function (app) {
     app.post('/login',account.login);
-    app.post('/signup',account.signup);
+    app.post('/signup',account.signup);;
 }
